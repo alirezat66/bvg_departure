1e58dc5f23770d4d5d7629d219a28780fe1233ad
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2025-08-21 21:48:28 +0200

    refactor app theme to use semantic colors for scaffold and add search bar theme

--- lib/core/presentation/theme/app_theme.dart
+++ lib/core/presentation/theme/app_theme.dart
@@ -14,9 +14,10 @@ ThemeData buildLightAppTheme() {
     useMaterial3: true,
     colorScheme: scheme,
     textTheme: appTextTheme,
-    scaffoldBackgroundColor: scheme.surface,
+    scaffoldBackgroundColor: semanticColors.bgPrimary,
     iconButtonTheme: iconButtonThemeData(semanticColors),
     appBarTheme: appBarTheme(semanticColors),
+    searchBarTheme: searchBarTheme(semanticColors),
     extensions: [semanticColors, AppSpacing.light, AppShape.light],
   );
 }
@@ -59,5 +60,58 @@ AppBarTheme appBarTheme(SemanticColors semanticColors) {
       statusBarIconBrightness: Brightness.dark,
       statusBarBrightness: Brightness.light,
     ),
+  );
+}
+
+SearchBarThemeData searchBarTheme(SemanticColors semanticColors) {
+  return SearchBarThemeData(
+    backgroundColor: WidgetStatePropertyAll(semanticColors.bgPrimary),
+    elevation: const WidgetStatePropertyAll(0),
+
+    shape: WidgetStatePropertyAll(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(28),
+        side: BorderSide(color: semanticColors.borderPrimary, width: 1),
+      ),
+    ),
+    hintStyle: WidgetStatePropertyAll(
+      appTextTheme.bodyLarge?.copyWith(color: semanticColors.fgTertiary),
+    ),
+    textStyle: WidgetStatePropertyAll(
+      appTextTheme.bodyLarge?.copyWith(color: semanticColors.fgPrimary),
+    ),
+    overlayColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.pressed)) {
+        return semanticColors.bgSecondary;
+      }
+      if (states.contains(WidgetState.hovered)) {
+        return semanticColors.bgSecondary;
+      }
+      if (states.contains(WidgetState.focused)) {
+        return semanticColors.bgSecondary;
+      }
+      return Colors.transparent;
+    }),
+    side: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.pressed)) {
+        return BorderSide(
+          color: semanticColors.interactionSecondaryPressed,
+          width: 1,
+        );
+      }
+      if (states.contains(WidgetState.hovered)) {
+        return BorderSide(
+          color: semanticColors.interactionSecondaryHovered,
+          width: 1,
+        );
+      }
+      if (states.contains(WidgetState.focused)) {
+        return BorderSide(
+          color: semanticColors.interactionSecondaryFocused,
+          width: 1,
+        );
+      }
+      return BorderSide(color: semanticColors.borderPrimary, width: 1);
+    }),
   );
 }
